f4fe5acd054ab3b41e43c132015bdd740dacadc8
U-AD\qiyi5000 <qiyi5000@LAW-D-1166.ad.colorado.edu>
2017-05-18 20:47:49 -0600

    cleanup

---
+++ Backend/src/Config/Keystore.java
@@ -0,0 +1,10 @@
+package Config;
+
+public class Keystore {
+	
+	public static final String keystoreLocation= "C:/Program Files/Java/jre1.8.0_51/bin/aes-keystore.jck";
+	public static final String keystorePass="Lawstorepass123,,";
+	public static final String keystorealias="jceksaes";
+	public static final String keyPass="Lawkeypass123,,";
+
+}
---
+++ Backend/src/Config/MySQL.java
@@ -0,0 +1,9 @@
+package Config;
+
+public class MySQL {
+	public static final String Driver="com.mysql.jdbc.Driver";
+	public static final String url="jdbc:mysql://127.0.0.1:3306/ratemylandlord?verifyServerCertificate=false&useSSL=true"; 
+	public static final String username="root";
+	public static final String password="law123";
+
+}
